ad907662a5c2fd98a82d60546c8224cbf7af25f3
NatyNatur <naticcg@gmail.com>
2018-01-08 12:53:42 -0300

    Etapa 4: imprimiendo recetas destacadas en div 'list-recipes'

--- js/script-index.js
+++ js/script-index.js
@@ -2,10 +2,11 @@ $(document).ready( function(){
 
 	$('.js-back').hide();
 
+//Llamo a la función que imprime un párrafo
+	printNews();
+
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
-
-	printNews();
 });
 
 	/*
@@ -20,15 +21,12 @@ $(document).ready( function(){
 * marcado el atributo "highlighted" como TRUE
 * el objeto, si tiene el atributo "highlighted", siempre está como true
 */
-var recipe = [];
 
 function renderHighlightedRecipes(recipesArray) {
 	console.log('Recipes: ', recipesArray);
 	for (var i = 0; i < recipesArray.length; i++) {
 		if (recipesArray[i].highlighted){
 			console.log([i]);
-			recipe.push(recipesArray[i].highlighted);
-			console.log(recipe);
 			renderRecipe(recipesArray[i]);
 		}
 	}
@@ -39,13 +37,25 @@ function renderHighlightedRecipes(recipesArray) {
 * marcado el atributo "highlighted" como TRUE
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-recipe.html"
+* el argumento 'recipe' recibe el valor de 'recipesArray[i]', no es 
+* necesario darte la posición nuevamente
 */
+
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
-
+		$('.list-recipes').append('<a class="item-recipe" href="#">' +
+				'<span class="attribution">' +
+					'<span class="title-recipe">' + recipe.title + '</span>' +
+					'<span class="metadata-recipe">' +
+					'<span class="author-recipe">' + recipe.source.name + '</span>' +
+					'<span class="bookmarks-recipe">' +
+					'<span class="icon-bookmark"></span>' +
+					'</span>' +
+				'</span>'+
+			'</span>'+
+			'<img src="img/recipes/640x800/'+ recipe.name +'.jpg"</>' +
+			'</a>')
 }
-
-
 
 /*
 * Función que se encarga de pintar todas las actividades
